f89ace24a5bd66777414710ebf3536504be85e92
faiez_benamar <38057044+benamarfaiez@users.noreply.github.com>
2025-11-20 14:20:38 +0100

    cmt

--- src/App.tsx
+++ src/App.tsx
@@ -18,18 +18,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => handlerCount()}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
   )
+
+  function handlerCount(): void {
+    return setCount((count) => count + 1)
+  }
 }
 
 export default App
