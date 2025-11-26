8005df9a3cb05d72f8a461552568381cef296e8f
faiez_benamar <38057044+benamarfaiez@users.noreply.github.com>
2025-11-26 02:12:34 +0100

    update data certifications

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -9,85 +9,85 @@ export const certifications: Certification[] = [
     {
         title: "React JS",
         issuer: "Udemy / Coursera",
-        date: "2023",
-        credentialUrl: "https://www.udemy.com/certificate/",
+        date: "Novembre 2025",
+        credentialUrl: "https://cursa.app/fr/mon-certificat/cert4a927c59cfdf8be3109a23855c9e1896",
     },
     {
         title: "Angular 12 .Net core web API & Microsoft SQL full stack app",
         issuer: "Udemy",
-        date: "2023",
-        credentialUrl: "https://www.udemy.com/certificate/",
+        date: "Juillet 2021",
+        credentialUrl: "https://udemy-certificate.s3.amazonaws.com/image/UC-a681e4dd-6815-479e-beb3-ba97ba7e9cab.jpg",
     },
     {
         title: "Business Analysis & Process Management",
         issuer: "Coursera",
-        date: "2022",
-        credentialUrl: "https://www.coursera.org/account/accomplishments/certificate/"
+        date: "Mai 2024",
+        credentialUrl: "https://www.coursera.org/account/accomplishments/verify/9HDYARW3FGMV"
     },
     {
         title: "Web Security",
-        issuer: "Udemy",
-        date: "2023",
-        credentialUrl: "https://www.udemy.com/certificate/"
+        issuer: "IBM",
+        date: "April 2022",
+        credentialUrl: "https://www.credly.com/badges/d19ee835-15fa-4ff4-8f8e-8a6bcaeaa8fc"
     },
     {
         title: "API",
-        issuer: "Postman",
-        date: "2023",
-        credentialUrl: "https://badgr.com/public/assertions/"
+        issuer: "IBM",
+        date: "April 2022",
+        credentialUrl: "https://www.credly.com/badges/ade52373-6610-4525-9160-66e4c5334647"
     },
     {
         title: "Git",
-        issuer: "Udemy",
-        date: "2022",
-        credentialUrl: "https://www.udemy.com/certificate/"
+        issuer: "IBM",
+        date: "April 2022",
+        credentialUrl: "https://www.credly.com/badges/181fcbcb-d270-4bbb-bd08-907cf7c06f67"
     },
     {
         title: "Cloud Intro",
-        issuer: "AWS / Azure",
-        date: "2023",
-        credentialUrl: "https://www.aws.training/transcript/"
+        issuer: "IBM",
+        date: "April 2022",
+        credentialUrl: "https://www.credly.com/badges/badfb93e-6402-4ca0-9e33-b7e0e39046f2"
     },
     {
         title: "Pentesting and security web application",
         issuer: "Udemy",
-        date: "2023",
-        credentialUrl: "https://www.udemy.com/certificate/"
+        date: "Aout 2021",
+        credentialUrl: "https://www.udemy.com/certificate/UC-28a41f64-5dd2-41e2-8e0d-1aaf0db4b36c/"
     },
     {
         title: "ASP .Net MVC & entity Framework",
         issuer: "Udemy",
-        date: "2022",
-        credentialUrl: "https://www.udemy.com/certificate/"
+        date: "Aout 2021",
+        credentialUrl: "https://udemy-certificate.s3.amazonaws.com/image/UC-c709bdb1-0503-4c45-b69b-3164a3b35051.jpg"
     },
     {
         title: "Responsive Web Design",
-        issuer: "FreeCodeCamp",
-        date: "2022",
-        credentialUrl: "https://www.freecodecamp.org/certification/"
+        issuer: "Coursera",
+        date: "Novembre 2020",
+        credentialUrl: "https://www.coursera.org/account/accomplishments/verify/XKFZA7UUFCYH"
     },
     {
         title: "Front-End Web UI Frameworks and Tools: Bootstrap 4",
         issuer: "Coursera",
-        date: "2022",
-        credentialUrl: "https://www.coursera.org/account/accomplishments/certificate/"
+        date: "Septembre 2020",
+        credentialUrl: "https://www.coursera.org/account/accomplishments/verify/R4GMJYYE3VMG"
     },
     {
         title: "Agile Software Development",
         issuer: "Coursera",
-        date: "2022",
-        credentialUrl: "https://www.coursera.org/account/accomplishments/certificate/"
+        date: "Aout 2020",
+        credentialUrl: "https://www.coursera.org/account/accomplishments/verify/TNDQYUPSV5VT"
     },
     {
         title: "JS Frameworks: Angular",
         issuer: "Coursera",
-        date: "2023",
-        credentialUrl: "https://www.coursera.org/account/accomplishments/certificate/"
+        date: "Aout 2020",
+        credentialUrl: "https://www.coursera.org/account/accomplishments/verify/VA24E6BUJ6ZG"
     },
     {
         title: "JavaScript",
-        issuer: "HackerRank / SoloLearn",
-        date: "2022",
-        credentialUrl: "https://www.hackerrank.com/certificates/"
+        issuer: "Udemy",
+        date: "Mai 2022",
+        credentialUrl: "https://www.udemy.com/certificate/UC-9f703df2-1b65-402d-993f-bb8f262987c8/"
     }
 ];
